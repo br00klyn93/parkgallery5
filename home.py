c9ec62a97c8e00cd0cee6c4a49c6151b67a722e5
br00klyn93 <bmclaury93@gmail.com>
2019-07-25 17:46:53 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, redirect
 from flask import make_response, Response
 
+app = Flask(__name__)
+
 @app.route('/')
 def main():
     return render_template("index.html")
